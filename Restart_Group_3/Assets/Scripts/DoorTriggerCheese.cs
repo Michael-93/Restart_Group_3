46c6d7200a3760c4b5a23616f25ca1477b027fbd
Michael-93 <78730532+Michael-93@users.noreply.github.com>
2021-02-17 12:26:45 +0000

    maze (Progress)

---
+++ Restart_Group_3/Assets/Scripts/DoorTriggerCheese.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DoorTriggerCheese : MonoBehaviour
+{
+    public bool cheeserequirementmet = false;
+    public static int cheeserequirement = 5;
+    public static int CheeseCount;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    public float x;
+    public Transform target;
+    public float Doormovetime;
+    public float TimeDoorStops;
+
+    [SerializeField]
+    GameObject door;
+    bool isOpened = false;
+
+        private void Update()
+        {
+
+        if (Pickup.CheeseCount >= cheeserequirement)
+            {
+                cheeserequirementmet = true;
+                isOpened = true;
+        }
+
+        if (cheeserequirementmet == true && isOpened == true)
+            {
+                
+            Vector3 targetPosition = door.transform.position + new Vector3(0, x, 0);
+            door.transform.position = Vector3.SmoothDamp(door.transform.position, targetPosition, ref velocity, smoothTime);
+            Doormovetime += Time.deltaTime;
+            if (Doormovetime >= TimeDoorStops)
+            {
+                isOpened = false;
+                Destroy(door);
+
+            }
+        }
+    }
+    }
+
---
+++ Restart_Group_3/Assets/Scripts/Pickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+
+public class Pickup : MonoBehaviour
+{
+    public GameObject Cheese;
+    public static int CheeseCount;
+    public TextMeshProUGUI CheeseText;
+    public AudioClip EatSound;
+
+    private void Start()
+    {
+        CheeseCount = 0;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+        {
+            Cheese = GameObject.FindGameObjectWithTag("Cheese");
+
+            if (gameObject != null)
+            {
+                AudioSource.PlayClipAtPoint(EatSound, other.transform.position);
+                CheeseCount++;
+                CheeseText.text = CheeseCount.ToString();
+                Destroy(gameObject);
+
+            }
+        }
+
+    }
+
